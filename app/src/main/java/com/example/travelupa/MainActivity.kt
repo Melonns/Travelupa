20dd954a613bca31cdda1b5cfe0abe3ddcb7b072
Melon <alvianprasetio24@gmail.com>
2025-09-02 10:41:11 +0700

    init project

---
+++ app/src/main/java/com/example/travelupa/MainActivity.kt
@@ -0,0 +1,128 @@
+package com.example.travelupa
+
+import ads_mobile_sdk.h4
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.lazy.items
+import androidx.activity.ComponentActivity
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import com.example.travelupa.ui.theme.TravelupaTheme
+import kotlin.coroutines.Continuation
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TravelupaTheme {
+                RekomendasiTempatScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Text(
+                text = "Selamat Datang di Travelupa!",
+                style = MaterialTheme.typography.h4,
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "Solusi buat kamu yang lupa kemana-mana",
+                style = MaterialTheme.typography.h6,
+                textAlign = TextAlign.Center
+            )
+        }
+
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .width(360.dp)
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 16.dp)
+        ) {
+            Text(text = "Mulai")
+        }
+    }
+}
+
+data class TempatWisata(val nama : String, val deskripsi : String, val gambar : Int)
+val daftarTempatWisata = listOf(
+    TempatWisata(
+        "Tumpak Sewu",
+        "Air Terjun Cantik di Jawa Timur",
+        R.drawable.tumpak_sewu
+    ) ,
+    TempatWisata(
+        "Gunung Bromo",
+        "Matahari Terbitnya Bagus Banget",
+        R.drawable.bromo
+    )
+)
+
+@Composable
+fun RekomendasiTempatScreen(){
+    LazyColumn(modifier = Modifier.padding(horizontal = 24.dp, vertical = 38.dp)) {
+        items(daftarTempatWisata){
+            tempat -> TempatItem(tempat)
+        }
+    }
+}
+
+@Composable
+fun TempatItem(tempat : TempatWisata){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+            .background(MaterialTheme.colors.surface),
+        elevation = 4.dp
+    ){
+        Column(modifier = Modifier.padding(16.dp)){
+            Image(
+                painter = painterResource(id = tempat.gambar),
+                contentDescription = tempat.nama,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = tempat.nama,
+                style = MaterialTheme.typography.h6,
+                modifier = Modifier.padding(bottom = 8.dp, top = 12.dp)
+            )
+            Text(
+                text = tempat.deskripsi,
+                style = MaterialTheme.typography.body2,
+                modifier = Modifier.padding(top = 4.dp)
+            )
+        }
+    }
+}
